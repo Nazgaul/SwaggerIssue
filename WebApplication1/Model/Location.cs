8e0a6d130d72196a33eedbba0985250983156ab9
Ram <ram.y@outlook.com>
2018-03-06 11:07:30 +0200

    new file

---
+++ WebApplication1/Model/Location.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Model
+{
+    public class Location
+    {
+        public float Long { get; set; }
+        public float Lat { get; set; }
+    }
+}
--- WebApplication1/Model/Request1.cs
+++ WebApplication1/Model/Request1.cs
@@ -5,10 +5,4 @@
         public string SomeType { get; set; }
         public Location Location { get; set; }
     }
-
-    public class Location
-    {
-        public float Long { get; set; }
-        public float Lat { get; set; }
-    }
 }
