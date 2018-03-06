a59978da2b437bba37ec64f436b0a0d440e940ed
Ram <ram.y@outlook.com>
2018-03-06 11:05:56 +0200

    Add project files.

---
+++ WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Model;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        /// <summary>
+        /// Some test
+        /// </summary>
+        /// <param name="model">the model that make a problem</param>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<string>), 200)]
+        public IEnumerable<string> Get([FromQuery] Request1 model)
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+       
+    }
+}
---
+++ WebApplication1/Model/Request1.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1.Model
+{
+    public class Request1
+    {
+        public string SomeType { get; set; }
+        public Location Location { get; set; }
+    }
+
+    public class Location
+    {
+        public float Long { get; set; }
+        public float Lat { get; set; }
+    }
+}
