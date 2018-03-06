2b27e827a20fffb5502c16161907754260be4d85
Ram <ram.y@outlook.com>
2018-03-06 11:16:36 +0200

    Added default value

--- WebApplication1/Model/Location.cs
+++ WebApplication1/Model/Location.cs
@@ -1,8 +1,12 @@
-﻿namespace WebApplication1.Model
+﻿using System.ComponentModel;
+
+namespace WebApplication1.Model
 {
     public class Location
     {
+        [DefaultValue(0f)]
         public float Long { get; set; }
+        [DefaultValue(0f)]
         public float Lat { get; set; }
     }
 }
